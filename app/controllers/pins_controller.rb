b56e6b7959a726dd6bb2601a7951e04913e0d5b4
unknown <hello@psdworkshop.com>
2015-07-26 00:27:02 +0200

    Added named route for show_by_name

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,9 +3,14 @@ class PinsController < ApplicationController
   def index
     @pins = Pin.all
   end
-  
+
   def show
     @pins = Pin.find(params[:id])
   end
+  
+  def show_by_name
+	@pins = Pin.find_by_slug!(params[:slug])
+	render :show
+  end 
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pins#index'
 
+  get "pins/name-:slug" => "pins#show_by_name", as: 'pin_by_name'
+  
   resources :pins
 
   get '/library' => 'pins#index'
   
+ 
   # get 'pins/:id' => 'pins#show'
 	
   # Example of regular route:
